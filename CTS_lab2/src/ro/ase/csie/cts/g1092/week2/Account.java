f88ffe70b38c5314be1d706308144f2f82db38f6
Flavius Calina <Flavius Calina@192.168.0.108>
2021-03-02 11:29:56 +0200

    Final Commit S2

--- CTS_lab2/src/ro/ase/csie/cts/g1092/week2/Account.java
+++ CTS_lab2/src/ro/ase/csie/cts/g1092/week2/Account.java
@@ -1,8 +1,10 @@
 package ro.ase.csie.cts.g1092.week2;
 
+import ro.ase.csie.cts.g1092.week2.exception.InsufficientFundsException;
+
 public abstract class Account {
 	public abstract double getBalance();
 	public abstract void deposit(double amount);
-	public abstract void transfer(Account destination, double ammount);
-	public abstract void withdraw(double amount);
+	public abstract void transfer(Account destination, double ammount) throws InsufficientFundsException;
+	public abstract void withdraw(double amount) throws InsufficientFundsException;
 }
