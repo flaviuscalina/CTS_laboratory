8b7eec181bc63339ace1f22e934c0101d1db2054
Flavius Calina <Flavius Calina@192.168.0.108>
2021-03-02 11:31:19 +0200

    Final Commit Week2

---
+++ CTS_lab2/src/ro/ase/csie/cts/g1092/week2/CurrentAccount.java
@@ -0,0 +1,39 @@
+package ro.ase.csie.cts.g1092.week2;
+
+import ro.ase.csie.cts.g1092.week2.exception.IllegalTransferException;
+import ro.ase.csie.cts.g1092.week2.exception.InsufficientFundsException;
+
+public class CurrentAccount extends BankAccount {
+	
+	public static final double MAX_CREDIT = 5000;
+	
+	
+	public  CurrentAccount(String IBAN) {
+		super(CurrentAccount.MAX_CREDIT,IBAN);
+	}
+	
+	@Override
+	public void deposit(double amount) {
+		this.balance += amount;
+
+	}
+
+	@Override
+	public void transfer(Account destination, double ammount) throws InsufficientFundsException {
+		//if(this==destination)
+			//throw new IllegalTransferException();
+		//this.withdraw(amount);
+		//destination.deposit(amount);
+		
+	}
+
+	@Override
+	public void withdraw(double amount) throws InsufficientFundsException {
+		
+		if(amount > this.balance)
+			throw new InsufficientFundsException("You don't have the amount");
+		else
+			this.balance -= amount;
+	}
+
+}
