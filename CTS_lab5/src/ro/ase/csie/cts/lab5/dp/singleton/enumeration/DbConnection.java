23d20ff26636b07896f0ee5de087d4d89f7bd589
Flavius Calina <Flavius Calina@192.168.0.108>
2021-03-23 11:16:27 +0200

    Singleton Enum

--- CTS_lab5/src/ro/ase/csie/cts/lab5/dp/singleton/enumeration/DbConnection.java
+++ CTS_lab5/src/ro/ase/csie/cts/lab5/dp/singleton/enumeration/DbConnection.java
@@ -1,7 +1,11 @@
 package ro.ase.csie.cts.lab5.dp.singleton.enumeration;
 
 //implement the singleton as an enumeration
-
+//"Singleton enum"
 public enum DbConnection {
-
+Instance;
+	
+	private DbConnection() {
+        System.out.println("Enum implementation");
+    }
 }
---
+++ CTS_lab5/src/ro/ase/csie/cts/lab5/dp/singleton/enumeration/TestSingletonEnum.java
@@ -0,0 +1,11 @@
+package ro.ase.csie.cts.lab5.dp.singleton.enumeration;
+
+public class TestSingletonEnum {
+
+	public static void main(String[] args) {
+
+		System.out.println(DbConnection.Instance);
+
+	}
+
+}
